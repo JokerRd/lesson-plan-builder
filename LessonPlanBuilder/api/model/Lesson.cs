c6cfaaba80da49d73870ef1ee61fb13d52d2c3dd
Евгений Куликов <ekueku2001@icloud.com>
2021-12-13 21:22:49 +0500

    add Lesson.cs

---
+++ LessonPlanBuilder/api/model/Lesson.cs
@@ -0,0 +1,14 @@
+﻿namespace LessonPlanBuilder.api.model
+{
+	public class Lesson
+	{
+		public readonly Subject Subject;
+		public readonly int Id; 
+		
+		public Lesson(Subject subject, int id)
+		{
+			Subject = subject;
+			Id = id;
+		}
+	}
+}
