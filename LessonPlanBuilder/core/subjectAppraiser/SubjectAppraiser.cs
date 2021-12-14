9a6397cd8d36bed85e60c099d3e3f29ed13a413a
Евгений Куликов <ekueku2001@icloud.com>
2021-12-15 01:46:18 +0500

    Сделал оценщика предметов

---
+++ LessonPlanBuilder/core/subjectAppraiser/SubjectAppraiser.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using LessonPlanBuilder.api.model;
+using LessonPlanBuilder.api.model.enums;
+
+namespace LessonPlanBuilder.core.subjectAppraiser
+{
+	public static class SubjectAppraiser
+	{
+		/// <summary>
+		/// Чем меньше значение, тем сложнее поставить предмет в рассписание
+		/// </summary>
+		public static double AppraiseSubject(Subject subject)
+		{
+			return (
+				from day in Enumerable.Range(0, 7)
+				from lesson in Enumerable.Range(0, 8)
+				where subject.Teacher.Schedule[day, lesson] != ScheduleCell.Impossible
+				from classroom in subject.AvailableClassrooms
+				where classroom.Schedule[day, lesson] != ScheduleCell.Impossible
+				select day).Count() / (double)subject.LessonsCount;
+		}
+	}
+}
