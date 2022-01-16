f633fbf2dd45ed8f701bb5c3499e160ebd86fa6b
Коньков Владислав <v.konkov@bftcom.com>
2022-01-16 23:17:01 +0500

    исправил тест

--- LessonPlanBuilder/test/TestTableManager.cs
+++ LessonPlanBuilder/test/TestTableManager.cs
@@ -1,13 +1,22 @@
 using LessonPlanBuilder.api.model;
-using LessonPlanBuilder.api.model.enums;
 using LessonPlanBuilder.core;
+using LessonPlanBuilder.core.services;
 using NUnit.Framework;
 
-namespace LessonPlanBuilder.test
-{
-    [TestFixture]
-    public class TestTableManager
-    {
+namespace LessonPlanBuilder.test;
 
+[TestFixture]
+public class TestTableManager
+{
+    private Shifter<Lesson> shifter;
+
+    private CellService<Lesson> cellService;
+
+    private RowService<Lesson> rowService;
+
+    [SetUp]
+    public void Init()
+    {
+        
     }
 }
